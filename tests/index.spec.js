6e03d82a2cae46d31da49d2ecdb2a751a3d725a8
queicherius <queicherius@gmail.com>
2016-02-12 02:42:23 +0100

    Better test timings

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -18,9 +18,9 @@ function timeoutPromise (ms, throwError = false) {
 describe('async-promises', () => {
   it('can work on promises in parallel', async () => {
     let promises = [
-      timeoutPromise(250),
-      timeoutPromise(250),
-      timeoutPromise(250)
+      timeoutPromise(100),
+      timeoutPromise(100),
+      timeoutPromise(100)
     ]
 
     let timestamps = await module.parallel(promises)
@@ -30,21 +30,21 @@ describe('async-promises', () => {
 
   it('can work on promises in series', async () => {
     let promises = [
-      timeoutPromise(250),
-      timeoutPromise(250),
-      timeoutPromise(250)
+      timeoutPromise(100),
+      timeoutPromise(100),
+      timeoutPromise(100)
     ]
 
     let timestamps = await module.series(promises)
-    expect(timestamps[1] - timestamps[0]).to.be.above(250)
-    expect(timestamps[2] - timestamps[1]).to.be.above(250)
+    expect(timestamps[1] - timestamps[0]).to.be.above(90)
+    expect(timestamps[2] - timestamps[1]).to.be.above(90)
   })
 
   it('can catch the promise error for parallel', async () => {
     let promises = [
-      timeoutPromise(250, true),
-      timeoutPromise(250, true),
-      timeoutPromise(250, true)
+      timeoutPromise(10, true),
+      timeoutPromise(10, true),
+      timeoutPromise(10, true)
     ]
 
     let err
@@ -59,9 +59,9 @@ describe('async-promises', () => {
 
   it('can catch the promise error for series', async () => {
     let promises = [
-      timeoutPromise(250, true),
-      timeoutPromise(250, true),
-      timeoutPromise(250, true)
+      timeoutPromise(10, true),
+      timeoutPromise(10, true),
+      timeoutPromise(10, true)
     ]
 
     let err
